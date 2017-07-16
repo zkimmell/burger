7644b281087adb5c8d78c37574e00c552981618e
zkimmell <zkimmell@yahoo.com>
2017-07-16 16:24:25 -0500

    Update comments

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override')
 
 var app = express();
-
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + '/public'));
 
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
 	extended: false
 }))
-
+// override with POST having ?_method=DELETE
 app.use(methodOverride('_method'))
 
 var exphbs = require('express-handlebars');
